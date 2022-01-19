e637bd60fb8fa6a7b2d7ed45ac162f0c42276bb9
mandeep Dhanju <mdhanju2@my.bcit.ca>
2022-01-19 00:26:16 -0800

    Allow user to upload image

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
-const express = require('express')
+require('dotenv').config()
+
+const express = require("express")
 const multer = require('multer')
-const upload = multer({ dest: 'images/' })
-const fs = require ('fs')
+const fs = require('fs')
+const path = require("path")
+
+const database = require('./database')
 
 const app = express()
 
-app.get("/", (req, res) => {
+const upload = multer({ dest: 'images/' })
 
-  res.send("🤗")
-})
+app.use(express.static(path.join(__dirname, "build")))
 
 app.get('/images/:imageName', (req, res) => {
-    // do a bunch of if statements to make sure the user is 
-    // authorized to view this image, then
-  
-    const imageName = req.params.imageName
-    const readStream = fs.createReadStream(`images/${imageName}`)
-    readStream.pipe(res)
-  })
+  const imageName = req.params.imageName
+  const readStream = fs.createReadStream(`images/${imageName}`)
+  readStream.pipe(res)
+})
+
+app.get("/api/images", async (req, res) => {
+  const images = await database.getImages()
+
+  res.send({images})
+})
+
+app.post("/api/images", upload.single('image'), async (req, res) => {
+  const imagePath = req.file.path
+  const description = req.body.description
+
+  const image = await database.addImage(imagePath, description)
+
+  res.send({image})
+})
 
 const port = process.env.PORT || 8080
-
-app.listen(8080, () => console.log("listening on port 8080"))
+app.listen(port, () =>  console.log(`listening on port ${port}`))
